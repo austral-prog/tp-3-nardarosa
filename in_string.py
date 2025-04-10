4fced5f58c79a25ef14fa2bb364022ee507b2bf9
nardarosa <nardarosa2050@gmail.com>
2025-04-10 16:13:32 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,6 +1,60 @@
 def check_vowels():
     # Código a implementar utilizando input.
+    name=input("cual es tu nombre?: ")
+    name=name.lower()
+    if "a" in name:
+        print("Contiene a: True")
+    else:
+        print("Contiene a: False")
+      
+    if "e" in name:
+        print("Contiene e: True")    
+    else:
+        print("Contiene e: False")
+        
+    if "i" in name:
+        print("Contiene i: True")   
+    else:
+        print("Contiene i: False")
+        
+    if "o" in name:
+        print("Contiene o: True")
+    else:
+        print("Contiene o: False")
+        
+    if "u" in name:    
+        print("Contiene u: True")    
+    else:
+        print("Contiene u: False")
+            
+    name2=input("Ingrese otro nombre: ")
+    name2=name2.lower()
+    
+    if "a" in name2:
+        print("Contiene a: True")
+    else:
+        print("Contiene a: False")
+      
+    if "e" in name2:
+        print("Contiene e: True")
+    else:
+        print("Contiene e: False")
+        
+    if "i" in name2:
+        print("Contiene i: True")   
+    else:
+        print("Contiene i: False")
+        
+    if "o" in name2:
+        print("Contiene o: True")
+    else:
+        print("Contiene o: False")
+        
+    if "u" in name2:    
+        print("Contiene u: True")
+    else:
+        print("Contiene u: False")
 
-
+check_vowels()
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
