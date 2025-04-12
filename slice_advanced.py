f0a0150137b5ced0d92a18709485eba19f4ec6a7
nardarosa <nardarosa2050@gmail.com>
2025-04-12 10:05:42 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -3,3 +3,10 @@ def slice_advanced():
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+    
+   text = input("ingrese un texto: ")
+   text=text.lower() 
+   print(text[4::2])
+    
+if __name__ == '__main__':
+    slice_advanced()
